22d9adb8c90c32ad5fd23f08d179b63d8149dbfb
Jun Dong <jund2511@gmail.com>
2020-11-14 10:44:30 -0600

    update old files and nonotes

--- Rank6/Simple_time_difference.py
+++ Rank6/Simple_time_difference.py
@@ -1,14 +1,11 @@
 def solve(arr):
     arr = sorted(int(m[:2]) * 60 + int(m[3:]) for m in set(arr))
-    print(arr)
     arr += [arr[0] + 1440]
     o = []
-    print(arr)
     for i in range(len(arr)-1):
         print(arr[5])
         o.append(arr[i+1] - arr[i])
-    print (o)
     h, m = divmod(max(arr[i + 1] - arr[i] - 1 for i in range(len(arr) - 1)), 60)
     print(h,m)
-    return "{:03}:{:03}".format(h, m)
+    return "{:02}:{:02}".format(h, m)
 print(solve(["21:14", "15:34", "14:51", "06:25", "15:30"]))
---
+++ Rank6/test2.py
@@ -0,0 +1,6 @@
+# follow the arrow, check next position, if its #
+# move , append "F"
+# if next position is #, turn
+# turn left, "L", right,"R", back "B"
+# check direction, make a direction to number 1,2,3,4
+# 
